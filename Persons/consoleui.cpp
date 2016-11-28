27f83442c087eae948279f878875dffa1c774562
Alex Gunnarsson <alexgunnarsson@clients-pool-1.nat.ru.is>
2016-11-28 19:49:47 +0000

    Update a service

--- Persons/consoleui.cpp
+++ Persons/consoleui.cpp
@@ -6,25 +6,39 @@ using namespace std;
 
 consoleUI::consoleUI(){}
 
+
 void consoleUI::run()
 {
+    bool valid = true;
     service turn;
     string command;
-    bool programloopbool = true;
-    do{
-    cout << "******************************************************************************" << endl;
-    cout << "Please enter one of the following commands:" << endl;
-    cout << "list - This command will list every single person in the system" << endl;
-    cout << "add -This command allows you to add a person to the list" << endl;
-    cout << "find -This command allows you to find a certain person in the list" << endl;
-    cout << "sorta -This command will list every single person in the system sorted alhpabaticly" << endl;
-    cout << "sortb -This command will list every single person in the system sorted by date of birth" << endl;
-    cout << "sortd -This command will list every single person in the system sorted by date of death" << endl;
-    cout << "sortg -This command will list every single person in the system sorted by their gender" << endl;
-    cout << "******************************************************************************" << endl;
-    cout << "command: ";
-    cin >> command;
-    turn.setCommand(command);
-    cout << endl;
-    }while(turn.displayList());
+    do
+    {
+        cout << "******************************************************************************" << endl;
+        cout << "Please enter one of the following commands:" << endl;
+        cout << "list - This command will list every single person in the system" << endl;
+        cout << "add -This command allows you to add a person to the list" << endl;
+        cout << "find -This command allows you to find a certain person in the list" << endl;
+        cout << "sorta -This command will list every single person in the system sorted alhpabaticly" << endl;
+        cout << "sortb -This command will list every single person in the system sorted by date of birth" << endl;
+        cout << "sortd -This command will list every single person in the system sorted by date of death" << endl;
+        cout << "sortg -This command will list every single person in the system sorted by their gender" << endl;
+        cout << "******************************************************************************" << endl;
+        cout << "command: ";
+        cin >> command;
+
+        turn.setCommand(command);
+        cout << endl;
+        valid = turn.validList();
+        if(valid)
+        {
+            turn.displayList();
+        }
+     }while(keepGoing(turn));
+}
+bool keepGoing(service turn)
+{
+    cout << "If you would like to continue, please enter (y/n): ";
+    cin >> turn._keepGoing;
+    return turn._keepGoing;
 }
--- Persons/service.h
+++ Persons/service.h
@@ -7,20 +7,22 @@ class service
 {
 public:
     service();
-    vector<Person> sort_alphabetically();
     void initalize_list();
     void swap(Person& a, Person& b);
-    vector<Person> get_list();
+    void setCommand(string command);
     bool displayList();
+    bool validList();
+    vector<Person> sort_alphabetically();
+    vector<Person> get_list();
     vector<Person> sort_BirthYear();
     vector<Person> sort_death();
     vector<Person> sort_gender();
-    void setCommand(string command);
 
 
 private:
     vector<Person> listV;
     string _command;
+    bool _keepGoing;
 };
 
 #endif // SERVICE_H
