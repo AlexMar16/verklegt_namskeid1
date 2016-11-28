c67a13922fece8795740649f8b17b7ea63261968
hrafnlogi <krummi3@gmail.com>
2016-11-28 14:12:51 +0000

    Main menu text nearly complete

--- Persons/consoleui.cpp
+++ Persons/consoleui.cpp
@@ -8,12 +8,56 @@ consoleUI::consoleUI()
 {
 
 }
+/*
+void dlist(string stuff)
+{
+    //notum kannski
+}
+*/
+void displaylist(string command)
+{
+    if(command == "list")
+    {
+        cout << "List! " << endl;
+    }
+    else if(command == "add")
+    {
+        cout << "ADd!" << endl;
+    }
+    else if(command == "find")
+    {
+        cout << "find!" << endl;
+    }
+    else if(command == "sorta")
+    {
+        cout << "soarta!" << endl;
+    }
+    else if(command == "sortb")
+    {
+        cout << "sortb!" << endl;
+    }
+    else if(command == "sortd")
+    {
+        cout << "sortd!" << endl;
+    }
+    else if(command == "sortg")
+    {
+        cout << "sortg!" << endl;
+    }
+    else
+    {
+        cout << "Invalid input, please try again" << endl;
+    }
+}
 
 void consoleUI::run()
 {
+    string command;
     bool going = true;
+
     while(going)
     {
+        cout << "******************************************************************************" << endl;
         cout << "Please enter one of the following commands:" << endl;
         cout << "list - This command will list every single person in the system" << endl;
         cout << "add -This command allows you to add a person to the list" << endl;
@@ -22,10 +66,15 @@ void consoleUI::run()
         cout << "sortb -This command will list every single person in the system sorted by date of birth" << endl;
         cout << "sortd -This command will list every single person in the system sorted by date of death" << endl;
         cout << "sortg -This command will list every single person in the system sorted by their gender" << endl;
-
-
-        string command;
+        cout << "******************************************************************************" << endl;
+        cout << "command: ";
         cin >> command;
+        displaylist(command);
+
+
+
+
+
     }
 
 }
