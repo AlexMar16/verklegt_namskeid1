5502b9f15d0b1d8570a37542a9ffba1f5adc380d
BjarkiHrafn <bjarki15@ru.is>
2016-11-29 14:32:58 +0000

    laga lgaga'

--- Persons/person.cpp
+++ Persons/person.cpp
@@ -12,24 +12,19 @@ fstream& operator >> (fstream& in, const Person& rhs)
     in >> cool;
     return in;
 }
-<<<<<<< HEAD
+
 ostream& operator << (ostream& out, const Person& rhs);
-=======
-<<<<<<< HEAD
+
 ostream& operator << (ostream& out, const Person& rhs)
-=======
-<<<<<<< HEAD
+
 
 string Person::get_name()
 {
     return _name;
 }
-ofstream& operator << (ofstream& out, Person rhs)
-=======
-ofstream& operator << (ofstream& out, const Person& rhs);
->>>>>>> aba2a8b3fd31800005e0d9d5922ec88d0c2ed0b5
->>>>>>> f4a8f42a7c9f2e431ff3b80fbe698e6106ba6dcc
->>>>>>> 9a2babdae8a5415ef08989ebcd19814165985332
+
+
+ostream& operator << (ofstream& out, const Person& rhs);
 {
     out << "Name: " << rhs._firstName << " " << rhs._secondName << " ";
     if(rhs._thirdName != "0")
@@ -39,36 +34,29 @@ ofstream& operator << (ofstream& out, const Person& rhs);
     out << endl;
     out << "Gender: " << rhs._gender << endl;
     out << "Born: " << rhs._birthYear << endl;
-    if(rhs._deathYear!= 0)
-    {
-        out << "Died: " << rhs._deathYear << endl;
-    }
-    else
-    {
-        out << "Still alive!" << endl;
-    }
-    return out;
+
+        if(rhs._deathYear!= 0)
+        {
+          out << "Died: " << rhs._deathYear << endl;
+        }
+            else
+            {
+              out << "Still alive!" << endl;
+            }
+                return out;
 }
-<<<<<<< HEAD
-    ostream& operator << (ostream& out, vector<Person>& rhs)
-    {
+
+ostream& operator << (ostream& out, vector<Person>& rhs)
+{
         for(size_t i = 0; i < rhs.size(); i++)
         {
             out << rhs[i];
         }
         return out;
-    }
+}
 
-=======
-<<<<<<< HEAD
+
 string Person::get_name() const
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a2babdae8a5415ef08989ebcd19814165985332
-string Person::get_name()
->>>>>>> f4a8f42a7c9f2e431ff3b80fbe698e6106ba6dcc
 {
     string fullName;
     fullName = _firstName + " " +  _secondName;
@@ -90,4 +78,3 @@ int Person::getDeath() const
 {
     return _deathYear;
 }
->>>>>>> aba2a8b3fd31800005e0d9d5922ec88d0c2ed0b5
