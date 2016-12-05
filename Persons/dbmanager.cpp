87cad44f057920c3052fe78dd61a243487d7d850
Alex Gunnarsson <alex16@ru.is>
2016-12-05 16:46:23 +0000

    Prenta

--- Persons/dbmanager.cpp
+++ Persons/dbmanager.cpp
@@ -1,25 +1,24 @@
 #include "dbmanager.h"
 #include <QVariant>
 #include <QDebug>
+#include <QSqlError>
 
 DbManager::DbManager(const QString &path)
 {
     _db = QSqlDatabase::addDatabase("QSQLITE");
-    QString dbName = "ComputerScience.sqlite";
+    QString dbName = path;
     _db.setDatabaseName(dbName);
     _db.open();
 }
 void DbManager::print() const
 {
-    QString s = "SELECT * FROM Students";
+    QString s = "SELECT * FROM Computers";
     QSqlQuery query(_db);
     cout << query.isSelect() << endl;
     query.exec(s);
-    query.next();
     while (query.next())
     {
         string jee = query.value("Name").toString().toStdString();
-        cout << jee << endl << endl;
+        cout << jee << endl;
     }
-    cout << "nope" << endl;
 }
