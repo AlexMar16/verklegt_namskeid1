b7808869782c6cceb17996ec68af6cd987009594
Alex Gunnarsson <alex16@ru.is>
2016-12-05 16:54:57 +0000

    Virkar

--- Persons/dbmanager.cpp
+++ Persons/dbmanager.cpp
@@ -9,16 +9,20 @@ DbManager::DbManager(const QString &path)
     QString dbName = path;
     _db.setDatabaseName(dbName);
     _db.open();
+    getPersons();
 }
 void DbManager::print() const
 {
     QString s = "SELECT * FROM Computers";
     QSqlQuery query(_db);
-    cout << query.isSelect() << endl;
     query.exec(s);
     while (query.next())
     {
         string jee = query.value("Name").toString().toStdString();
         cout << jee << endl;
     }
+}
+void DbManager::getPersons()
+{
+
 }
