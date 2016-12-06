e4617e3f86a4e08b89aca93e029a072e8ade0003
BjarkiHrafn <bjarki15@ru.is>
2016-12-06 14:40:24 +0000

    er i getcomputers

--- Persons/dbmanager.cpp
+++ Persons/dbmanager.cpp
@@ -1,5 +1,7 @@
 #include "dbmanager.h"
 #include <QVariant>
+#include <QSqlQuery>
+
 
 DbManager::DbManager(){}
 
@@ -10,6 +12,7 @@ DbManager::DbManager(const QString &path)
     _db.setDatabaseName(dbName);
     _db.open();
     getPersons();
+
 }
 DbManager::~DbManager() {_db.close();}
 
@@ -51,4 +54,34 @@ void DbManager::insertIntoComputer(const Computer &input)
     cout << "type: " << input.getType() << endl;
     cout << "year: " << input.getYearBuilt() << endl;
     cout << "built? " << input.getBuilt() << endl;
+    QString qsName = QString::fromStdString(input.getName());
+    QString qsType= QString::fromStdString(input.getType());
+    QString qsBuilt = QString::fromStdString(input.getBuilt());
+    QString path = "C:/Users/Rabo/HR/onn1/Verklegt Namskeid/verklegt_namskeid1/ComputerScience.sqlite";
+    _db = QSqlDatabase::addDatabase("QSQLITE");
+    QString dbName = path;
+    _db.setDatabaseName(dbName);
+    _db.open();
+
+    if(_db.open())
+    {
+        cout << "opened!";
+        QSqlQuery qry;
+        qry.prepare("INSERT INTO Computers(Name, yearBuilt, Type, Built)"
+                    "VALUES(:C_Name,:C_yearBuilt,:C_Type,:C_Built");
+        qry.bindValue(":C_Name",qsName);
+        qry.bindValue(":C_yearBuilt",input.getYearBuilt());
+        qry.bindValue(":C_Type",qsType);
+        qry.bindValue(":C_Built",qsBuilt);
+        if( !qry.exec() )
+            //qDebug() << qry.lastError().text();
+            cout << "error inserting into database";
+        else
+            qDebug( "Inserted!" );
+        cout << "inserted! " << endl;
+    }
+    else
+    {
+        cout << "not open " << endl;
+    }
 }
