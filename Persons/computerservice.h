584ba77e54e3a5c92918aa6d96f0e7262434595a
Sindri Ingólfsson <sindrii16@ru.is>
2016-12-07 00:06:05 +0000

    ekkert

--- Persons/computerservice.h
+++ Persons/computerservice.h
@@ -14,17 +14,18 @@ public:
     Computer generateQuestion();
     vector<int> computerProperties();
     vector<Computer> findComputer(const string& name);
-    vector<Computer> getComputerList() const;
+    vector<Computer> getList() const;
     void addComputer(const Computer& input);
     void removeComputer(const Computer& input);
     void sortList(const string& command);
     void setProgram(const bool& input);
     void generateOptions(const Computer& correct, string& a, string& b, string& c, string& d);
     void reverseVector();
-    void closeDatabase();
-    //string assignSelection(string& answer, const string& a, const string& b, const string& c, const string& d);
-    //string genderCheck(const Computer& p);
-   // string aliveCheck(const Computer& p);
+    string getLastName(const string& name) const;
+    string toLower(const string &tolowerstring);
+    string assignSelection(string& answer, const string& a, const string& b, const string& c, const string& d);
+    string genderCheck(const Computer& p);
+    string aliveCheck(const Computer& p);
 
 private:
     bool _keepGoing;
@@ -37,8 +38,6 @@ private:
     vector<Computer> sortAlphabetically();
     vector<Computer> sortBuilt();
     vector<Computer> sortType();
-    vector<Computer> sortCreationYear();
-    DbManager _database;
     void removeFromDatabase(const string& name);
     void swap(Computer& a, Computer& b);
 };
