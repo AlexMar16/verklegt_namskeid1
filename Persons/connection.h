1d8f34b1d7cebc2fc1042f2bc5abf67555698083
hrafnlogi <krummi3@gmail.com>
2016-12-08 21:30:24 +0000

    connection set og get

--- Persons/connection.h
+++ Persons/connection.h
@@ -11,11 +11,17 @@ public:
     Connection();
     void setComputerName(const string& input);
     void setPersonName(const string& input);
+    void setComputerID(const int& input);
+    void setPersonID(const int& input);
+    int getComputerID();
+    int getPersonID();
     string getComputerName();
     string getPersonName();
 private:
     string _computerName;
     string _personName;
+    int _ComputerID;
+    int _PersonID;
 };
 
 #endif // CONNECTION_H
