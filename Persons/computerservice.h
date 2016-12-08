9160a23e59e9240dab169d76fa80aae86df47fc4
BjarkiHrafn <bjarki15@ru.is>
2016-12-08 15:06:40 +0000

    iejf

--- Persons/computerservice.h
+++ Persons/computerservice.h
@@ -1,40 +1,43 @@
 #ifndef COMPUTERSERVICE_H
 #define COMPUTERSERVICE_H
 #include "dbmanager.h"
-#include "generalclass.h"
+#include "computer.h"
 
 class computerService
 {
 public:
     computerService();
     bool lookForComputer(const string& name);
-    bool lookForDigits(const int& num);
+    bool getProgram() const;
+    bool dataFound() const;
     Computer findComputerExactly(const string& name);
+    Computer generateQuestion();
     vector<int> computerProperties();
     vector<Computer> findComputer(const string& name);
-    vector<Computer> findDigits(const int& num);
-    vector<Computer> getComputerList() const;
+    vector<Computer> getList() const;
     void addComputer(const Computer& input);
     void removeComputer(const Computer& input);
-    void sortComputerList(const string& command);
-    Computer generateAnswer();
-    string builtCheck(const Computer& c);
+    void sortList(const string& command);
+    void setProgram(const bool& input);
     void generateOptions(const Computer& correct, string& a, string& b, string& c, string& d);
-    string typeCheck(const Computer& c);
-    //void generateOptions(const Computer& correct, string& a, string& b, string& c, string& d);
     void reverseVector();
-    //string assignSelection(string& answer, const string& a, const string& b, const string& c, const string& d);
+    string getLastName(const string& name) const;
+    string toLower(const string &tolowerstring);
+    string assignSelection(string& answer, const string& a, const string& b, const string& c, const string& d);
+    string genderCheck(const Computer& p);
+    string aliveCheck(const Computer& p);
 
 private:
+    bool _keepGoing;
+    bool _valid;
+    bool _program;
     bool alreadyInDatabase(const string &name);
+    bool _dataFound;
     vector<Computer> _listComputer;
     vector<Computer> _listSearchedComputer;
-    vector<Computer> _listSearchedDigits;
     vector<Computer> sortAlphabetically();
     vector<Computer> sortBuilt();
     vector<Computer> sortType();
-
-    vector<Computer> sortCreationYear();
     void removeFromDatabase(const string& name);
     void swap(Computer& a, Computer& b);
 };
