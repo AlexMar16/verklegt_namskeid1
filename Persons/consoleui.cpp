a79ac8e432dd48ef6875f710160c69364b1b4089
BjarkiHrafn <bjarki15@ru.is>
2016-12-09 17:42:20 +0000

    findit

--- Persons/consoleui.cpp
+++ Persons/consoleui.cpp
@@ -181,9 +181,9 @@ void consoleUI::commandBoxConnect()
     cout << left  << setw(ASTERISK_WIDTH) << setfill(SPACE) << BARRIER << right << BARRIER<< endl;
     cout << left  << setw(ASTERISK_WIDTH) << setfill(SPACE) << "| add            - This command will allow you to add a connection."
          << right << BARRIER << endl;
-    cout << left  << setw(ASTERISK_WIDTH) << setfill(SPACE) << "| ctp            - This command will show connections from computers to persons."
+    cout << left  << setw(ASTERISK_WIDTH) << setfill(SPACE) << "| ctp            - This command will find connections from computers to persons."
          << right << BARRIER << endl;
-    cout << left  << setw(ASTERISK_WIDTH) << setfill(SPACE) << "| ptc            - This command will show connections from persons to computers."
+    cout << left  << setw(ASTERISK_WIDTH) << setfill(SPACE) << "| ptc            - This command will find connections from persons to computers."
          << right << BARRIER << endl;
     cout << left  << setw(ASTERISK_WIDTH) << setfill(SPACE) << "| remove         - This command will allow you to remove a connection."
          << right << BARRIER << endl;
